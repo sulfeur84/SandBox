e9c3eda641dbb267b732bf9ee64d9838e73dd783
sulfeur84 <sulfeur84@gmail.com>
2021-10-15 16:41:08 +0200

    Push du 15 Oct

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -7,25 +7,30 @@ public class Jump : MonoBehaviour
 {
     public float targetTime = 1f;
     public float Cadence ;
-    Rigidbody m_Rigidbody;
+    private Rigidbody m_Rigidbody;
+    private SphereCollider m_Collider;
+    private Transform m_Transform;
     private bool CanJump;
+    private bool CanCrouch;
     public float m_Thrust = 20f;
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<SphereCollider>();
+        m_Transform = GetComponent<Transform>();
     }
 
     void FixedUpdate()
     {
+        targetTime -= Time.deltaTime;
         
         if (Input.GetButton("Jump") && CanJump)
         {
             m_Rigidbody.AddForce(transform.up * m_Thrust);
             targetTime = Cadence;
         }
-        targetTime -= Time.deltaTime;
-
+        
         if (targetTime <= 0.0f)
         {
             CanJump = true;
